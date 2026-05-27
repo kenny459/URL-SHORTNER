15be7337634a4e9ee7257ac8d9cd6c470b92219a
kenny459 <kennedyuyanwanne@gmail.com>
2026-05-27 06:46:40 +0100

    debuging click count

--- client/src/hooks/useClickCount.ts
+++ client/src/hooks/useClickCount.ts
@@ -7,11 +7,19 @@ const useClickCount = (shortCode: string | null, initialCount: number) => {
     if (!shortCode) return;
 
     const apiBase = import.meta.env.VITE_API_BASE_URL;
-    const url = `${apiBase}/${shortCode}/live`;
+    const url = `${apiBase}/${shortCode}/live`; 
+     
+     console.log('Connecting to SSE:', url); 
 
-    const eventSource = new EventSource(url);
+    const eventSource = new EventSource(url); 
+     
+    eventSource.onopen = () => {
+    console.log('SSE connection opened'); // 👈 add this
+  };
 
-    eventSource.onmessage = (event) => {
+
+    eventSource.onmessage = (event) => { 
+       console.log('SSE message received:', event.data);
       const data = JSON.parse(event.data);
       if (data.clickCount !== undefined) {
         setClickCount(data.clickCount);
