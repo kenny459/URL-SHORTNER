48a62e75e8e5640906011463b6c8b69b710a0a2f
kenny459 <kennedyuyanwanne@gmail.com>
2026-05-27 06:18:01 +0100

    implemented real time click counts update

--- server/routes/url.routes.js
+++ server/routes/url.routes.js
@@ -4,7 +4,7 @@ import prisma from "../config/prisma.js";
 import { createUrl } from "../controllers/urlControllers.js"; 
 import { validate } from "../middlewares/validateurls.js"; 
 import { createUrlSchema } from "../schemas/url.schemas.js";
-
+import { broadcast, addClient, removeClient } from "../utils/sseManager.js";
 const router = Router();   
 
 router.post('/', validate(createUrlSchema), createUrl); 
@@ -22,7 +22,10 @@ router.get('/:code', async (req, res) => {
         where: { short_code: code },
         data: { click_count: { increment: 1 } },
         select: { long_url: true }
-      });
+      }); 
+       
+      broadcast(code, Number(updateResult.click_count)); 
+
       longUrl = updateResult.long_url;
     } catch (error) {
       if (error.code === 'P2025') {  // Record not found
@@ -52,6 +55,26 @@ router.get('/:code', async (req, res) => {
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+ 
+router.get('/:code/live', (req, res) => {
+  const { code } = req.params;
 
+  // Set SSE headers
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders();
+
+  // Send initial connection confirmation
+  res.write(`data: ${JSON.stringify({ connected: true })}\n\n`);
+
+  // Register this client
+  addClient(code, res);
+
+  // Clean up when client disconnects
+  req.on('close', () => {
+    removeClient(code, res);
+  });
+});
 
 export default router;
---
+++ server/utils/sseManager.js
@@ -0,0 +1,30 @@
+const clients = new Map(); 
+ 
+    export const addClient = (code, res) => { 
+        if (!clients.has(code)) {
+    clients.set(code, []);
+    }  
+     
+    clients.get(code).push(res);
+
+    } 
+     
+    export const removeClient = (code, res) => { 
+        if (clients.has(code)) return;  
+        const updatedClients = clients.get(code).filter(client => client !== res); 
+        
+        if (updatedClients.length === 0) {  
+            clients.delete(code);
+        } else {
+            clients.set(code, updatedClients);
+        }
+    } 
+     
+    export const broadcast = (code, clickCount) => { 
+        if (!clients.has(code)) return; 
+         
+        const message = `data: ${JSON.stringify({ clickCount })}\n\n`; 
+         
+        clients.get(code).forEach(res => res.write(message));
+
+    }
